646cee1beb4f5138e91d6bfcb16753d1c1652360
Nikita <s2015836@x.donstu.ru>
2021-12-13 22:58:34 +0300

    reorganize project

---
+++ arduinoFunctions/movementAF/movementAF.ino
@@ -0,0 +1,35 @@
+#define DIR_PIN_RIGHT 49
+#define PWM_PIN_RIGHT 2
+
+#define DIR_PIN_LEFT 51
+#define PWM_PIN_LEFT 3
+
+void setup() {
+  pinMode(DIR_PIN_RIGHT, OUTPUT);
+  pinMode(DIR_PIN_LEFT, OUTPUT);
+}
+
+void loop() {
+  rightEngineDriver(60);
+  leftEngineDriver(60);
+}
+
+void rightEngineDriver(int a) {
+    if (a >= 0) {
+      digitalWrite(DIR_PIN_RIGHT, LOW);
+      analogWrite(PWM_PIN_RIGHT, a);
+    } else if (a < 0) {
+      digitalWrite(DIR_PIN_RIGHT, HIGH);
+      analogWrite(PWM_PIN_RIGHT, 255 + a);
+    }
+}
+
+void leftEngineDriver(int a) {
+    if (a >= 0) {
+      digitalWrite(DIR_PIN_LEFT, LOW);
+      analogWrite(PWM_PIN_LEFT, a);
+    } else if (a < 0) {
+      digitalWrite(DIR_PIN_LEFT, HIGH);
+      analogWrite(PWM_PIN_LEFT, 255 + a);
+    }
+}
